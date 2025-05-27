ba1814ddacef2c7b0db60858d0b216a4a17f370a
maida <3857391+stardust5678@users.noreply.github.com>
2025-05-27 14:28:52 -0700

    nuke everything

--- app/forms.py
+++
@@ -1,4 +0,0 @@
-from django import forms
-
-class SecretMessageForm(forms.Form):
-    secret_message = forms.CharField(label="Enter your secret message", max_length=1000)
--- app/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class SecretMessage(models.Model):
-    token = models.CharField(max_length=40, unique=True, primary_key=True)
-    encrypted_message = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    expires_at = models.DateTimeField()
